99d720b823f45028ad9f524f1f89f1e6b8ae1903
aliance22 <andrealiance33@gmail.com>
2022-06-21 12:20:19 +0700

    new file

---
+++ display.py
@@ -0,0 +1,33 @@
+import pygame, sys, os, random
+from gamestart import GameStart
+from pygame.locals import *
+
+WINDOWHEIGHT = 600
+WINDOWWIDTH = 800
+
+class Display(GameStart):
+    
+    def __init__(self, mainClock, windowSurface, font):
+        self.mainClock = mainClock
+        self.windowSurface = windowSurface
+        self.font = font
+    
+    def show_display(self):
+        pygame.init()
+        
+    def waktu(self):
+        self.mainClock = pygame.time.Clock()
+        
+    def windows(self):
+        self.windowSurface = pygame.display.set_mode(WINDOWHEIGHT, WINDOWWIDTH)
+        pygame.display.set_caption('car race')
+        
+    def text(self):
+        self.font = pygame.font.SysFont(None, 30)
+    
+    def control(self):
+        return pygame.mouse.set_visible(False)
+    
+    def sound(self):
+        return pygame.mixer.music.load('Resources/music/music_car.wav')
+    laugh = pygame.mixer.Sound('Resources/music/music_laugh.wav')
--- gamestart.py
+++ gamestart.py
@@ -1,11 +1,24 @@
-import pygame, random, sys ,os,time
+import pygame, sys
 from pygame.locals import *
+
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOR = (0, 0, 0)
+FPS = 40
+BADDIEMINSIZE = 10
+BADDIEMAXSIZE = 40
+BADDIEMINSPEED = 8
+BADDIEMAXSPEED = 8
+ADDNEWBADDIERATE = 6
+PLAYERMOVERATE = 5
+count=3
 
 class GameStart:
     
     def __init__(self, quit, color):
         self.terminate = quit
-        self.tekscolor = color
+        self.TEXTCOLOR = color
         
     def terminate(self):
         pygame.quit()
@@ -28,7 +41,7 @@ class GameStart:
         return False
     
     def drawText(self, text, font, surface, x, y):
-        textobj = font.render(text, 1, self.tekscolor)
+        textobj = font.render(text, 1, self.TEXTCOLOR)
         textrect = textobj.get_rect()
         textrect.topleft = (x, y)
         surface.blit(textobj, textrect)
