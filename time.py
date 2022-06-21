789ba1b7c5a2718a24394c0e2be3705373a846b5
firgiansyahim <firgiansyahiqbalm@gmail.com>
2022-06-21 14:48:34 +0700

    game

--- time.py
+++ time.py
@@ -2,12 +2,12 @@ class time:
     
 pygame.mixer.music.stop()
     count=count-1
-    gameOverSound.play()
+        gameOverSound.play()
     time.sleep(1)
     if (count==0):
         laugh.play()
-    drawText('Game over', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
-    drawText('Press any key to play again.', font, windowSurface, (WINDOWWIDTH / 3) - 80, (WINDOWHEIGHT / 3) + 30)
+        drawText('Game over', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+        drawText('Press any key to play again.', font, windowSurface, (WINDOWWIDTH / 3) - 80, (WINDOWHEIGHT / 3) + 30)
     pygame.display.update()
     time.sleep(2)
     waitForPlayerToPressKey()
